53426a4e6f61182e30c0f8a28a59d8281d7d597a
liuyant <liuyant@zbj.com>
2020-07-21 10:04:56 +0800

    63-不同路径2-有障碍

--- algorithm/src/main/java/com/yanl/dp/UniquePathWithTrouble.java
+++ algorithm/src/main/java/com/yanl/dp/UniquePathWithTrouble.java
@@ -27,17 +27,14 @@ public class UniquePathWithTrouble {
         int m = obstacleGrid.length;
         int n = obstacleGrid[0].length;
         int[][] dp = new int[m][n];
-        for(int i = 0; i < m; i++){
-            if(obstacleGrid[i][0] == 0){
+        //初始化第一行和第一列为1---只能向右走或者向下走，如果存在障碍，则为0
+        for(int i = 0; i < m && obstacleGrid[i][0] == 0; i++){
                 dp[i][0] = 1;
-            }
         }
-        for(int i = 0; i < n; i++){
-            if(obstacleGrid[0][i] == 0){
+        for(int i = 0; i < n && obstacleGrid[0][i] == 0; i++){
                 dp[0][i] = 1;
-            }
         }
-
+        
         for(int i = 1; i < m; i++){
             for(int j = 1; j < n; j++){
                 if(obstacleGrid[i][j] == 0){
