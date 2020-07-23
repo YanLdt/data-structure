9510918f6d642f717ce16f964185303e25dc8d8e
liuyant <liuyant@zbj.com>
2020-07-23 17:07:52 +0800

    45-跳跃游戏2

--- algorithm/src/main/java/com/yanl/dp/JumpGameV2.java
+++ algorithm/src/main/java/com/yanl/dp/JumpGameV2.java
@@ -1,6 +1,7 @@
 package com.yanl.dp;
 
 /**
+ * leetcode 45 跳跃游戏2  贪心算法
  * 给定一个非负整数数组，你最初位于数组的第一个位置。
  * 数组中的每个元素代表你在该位置可以跳跃的最大长度。
  * 你的目标是使用最少的跳跃次数到达数组的最后一个位置。
@@ -18,16 +19,18 @@ public class JumpGameV2 {
         if(nums.length == 1){
             return 0;
         }
-        int reach = 0;
-        int nextReach = nums[0];
+        int reach = 0; //以当前跳跃步数所能到达的最远位置
+        int nextReach = nums[0]; //当前位置所能到达的最远位置
         int res = 0;
         for(int i = 0; i < nums.length; i++){
             nextReach = Math.max(i + nums[i], nextReach);
             if(nextReach >= nums.length - 1){
                 return res + 1;
             }
+            //到达了当前跳跃步数的边界，不得不跳
             if(i == reach){
                 res++;
+                //更新最远位置
                 reach = nextReach;
             }
         }
